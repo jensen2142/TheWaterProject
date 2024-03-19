3b7eb2f46fd40a2485815c640cdc238ee8c28683
jherm <jherm@JENSENSLAPTOP>
2024-03-19 09:15:11 -0600

    Add project files.

---
+++ TheWaterProject/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using SQLitePCL;
+using System.Diagnostics;
+using TheWaterProject.Models;
+using TheWaterProject.Models.ViewModels;
+
+namespace TheWaterProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IWaterRepository _repo;
+        public HomeController(IWaterRepository temp)
+        {
+            _repo = temp;
+        }
+        public IActionResult Index(int pageNum)
+        {
+            int pageSize = 2;
+            var blah = new ProjectsListViewModel
+            {
+                Projects = _repo.Projects
+                .OrderBy(x => x.ProjectName)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PaginationInfo = new PaginationInfo
+                {
+                    CurrentPage = pageNum,
+                    ItemsPerPage = pageSize,
+                    TotalItmes = _repo.Projects.Count()
+
+                }
+            };
+
+            return View(blah);
+        }
+
+    }
+}
